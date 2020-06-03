02d8a090650eb527ac7fd91e69bcd554ef4f1c7e
Jose Maria Samos <jsamos@cifpfbmoll.eu>
2020-06-03 22:10:17 +0200

    feat: Añadir función getFacturas
    Añadir función getFacturas que devuelve las facturas
    un cliente especificado

--- src/main/java/org/formacio/repositori/FacturesRepositori.java
+++ src/main/java/org/formacio/repositori/FacturesRepositori.java
@@ -1,5 +1,7 @@
 package org.formacio.repositori;
 
+import java.util.List;
+
 import org.formacio.domain.Factura;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -8,5 +10,8 @@ public interface FacturesRepositori extends CrudRepository<Factura, Long> {
 
 	@Query("select sum(lf.total) from Factura f join f.linies lf where f.client.nom = ?1")
 	public Number totalClient(String client);
+	
+	@Query("select f from Factura f where f.client.nom = ?1")
+	public List<Factura> getFactures(String nomClient);
 	
 }
