2280f2c91279c1145657f4f2b2fd47a6d07daef6
Jose Maria Samos <jsamos@cifpfbmoll.eu>
2020-06-03 21:58:42 +0200

    feat: Añadir función totalClient
    Añadir función totalClient que devuelve el total gastado
    por un cliente en específico

--- src/main/java/org/formacio/repositori/FacturesRepositori.java
+++ src/main/java/org/formacio/repositori/FacturesRepositori.java
@@ -1,10 +1,12 @@
 package org.formacio.repositori;
 
 import org.formacio.domain.Factura;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 
 public interface FacturesRepositori extends Repository<Factura, Long> {
 
-	//public Number totalClient(String client);
+	@Query("select sum(lf.total) from Factura f join f.linies lf where f.client.nom = ?1")
+	public Number totalClient(String client);
 	
 }
