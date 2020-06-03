19f887f0792ef29116238bab164ee47b9fab81ef
Jose Maria Samos <jsamos@cifpfbmoll.eu>
2020-06-03 22:24:11 +0200

    feat: Añadir funcionalidad a afegirProducte
    Añadir funcionalidad al método afegirProducte para
    que añada productos nuevos a una factura especificada

--- src/main/java/org/formacio/servei/FacturesService.java
+++ src/main/java/org/formacio/servei/FacturesService.java
@@ -1,11 +1,18 @@
 package org.formacio.servei;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
 import org.formacio.domain.Factura;
+import org.formacio.domain.LiniaFactura;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FacturesService {
 
+	@PersistenceContext
+	EntityManager em;
 	
 	/*
 	 * Aquest metode ha de carregar la factura amb id idFactura i afegir una nova linia amb les dades
@@ -15,8 +22,16 @@ public class FacturesService {
 	 * 
 	 * Per implementar aquest metode necessitareu una referencia (dependencia) a FacturesRepositori
 	 */
+	@Transactional
 	public Factura afegirProducte (long idFactura, String producte, int totalProducte) {
 		
-		return null;
+		Factura producteFactura = em.find(Factura.class, idFactura);
+		LiniaFactura liniaFactura = new LiniaFactura();
+		
+		liniaFactura.setProducte(producte);
+		liniaFactura.setTotal(totalProducte);
+		producteFactura.getLinies().add(liniaFactura);
+		
+		return producteFactura;
 	}
 }
